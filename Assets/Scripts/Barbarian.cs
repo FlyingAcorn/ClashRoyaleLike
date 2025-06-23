1c409764312acf935ddf55f5f455a46398ec52fb
Onur Rasna <154007465+FlyingAcorn@users.noreply.github.com>
2025-06-23 23:35:23 +0300

    Added Barbarian

---
+++ Assets/Scripts/Barbarian.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Barbarian : Agent
+{
+    [SerializeField] private MeshCollider hitCollider;
+    protected override IEnumerator Acting()
+    {
+        myAnimator.SetBool("isAttacking",true);
+        hitCollider.enabled = true;
+        transform.DOLookAt(target.transform.position,0.25f,AxisConstraint.Y);
+        yield return new WaitForSeconds(2.2f);
+        yield return new WaitForSeconds(entityClassType.attackSpeed);
+        if (FindClosestTarget()-target.ColliderOffset()> entityClassType.rangeRadius)
+        {
+            UpdateAgentState(AgentBehaviour.MovingToClosestTarget);
+        }
+        else
+        {
+            currentCoroutine = StartCoroutine(Acting());
+        }
+        yield return null;
+    }
+
+    public void Hit()
+    {
+        hitCollider.enabled = false;
+        myAnimator.SetBool("isAttacking",false);
+    }
+}
